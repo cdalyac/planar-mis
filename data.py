08ca13b79ceda302cb112b0a3fd37c21f2399f3c
Nick Magerko <nmagerko@gmail.com>
2015-05-10 03:16:20 -0500

    Data is now sorted

--- data.py
+++ data.py
@@ -5,12 +5,11 @@ import planarity
 import os
 
 # global declarations
-# note that ending_vertices is non-inclusive
 data = []
 
 starting_vertices = 3
-ending_vertices = 15
-trials = 10
+ending_vertices = 10
+trials = 5
 
 output_file = "data/output.txt"
 
@@ -25,8 +24,8 @@ def make_graph(vertices, edge_probability):
 
 	return G
 
-for vertices in range(starting_vertices, ending_vertices):
-	for i in range(trials):
+for vertices in range(starting_vertices, ending_vertices + 1):
+	for i in range(trials + 1):
 		# try to make a planar graph
 		# the edge probability's equation proceeds from trial-and-error
 		edge_probability = random.uniform(0, 0.0091*(vertices**2) - 0.1488*vertices + 0.8092)
@@ -40,6 +39,9 @@ for vertices in range(starting_vertices, ending_vertices):
 		G_nx = planarity.planarity_networkx.networkx_graph(G)
 		max_indep_set = len(nx.maximal_independent_set(G_nx))
 		data.append((vertices, G_nx.number_of_edges(), max_indep_set))
+
+# sort the data by n(G), then by e(G)
+data = sorted(data, key = lambda trial: (trial[0], trial[1]))
 
 # get rid of the old data before writing
 # then open the file, and print add the header
