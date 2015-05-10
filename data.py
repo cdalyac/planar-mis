fbaf97df2eed5d1977c84b609de06a72d0f13765
Nick Magerko <nmagerko@gmail.com>
2015-05-09 21:50:06 -0500

    Changed edge probability technique and outputted data to file

--- data.py
+++ data.py
@@ -2,24 +2,54 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 import planarity
+import os
 
+# global declarations
+# note that ending_vertices is non-inclusive
 data = []
 
+starting_vertices = 3
+ending_vertices = 15
+trials = 10
+
+output_file = "data/output.txt"
+
 def make_graph(vertices, edge_probability):
+	"""
+	Returns a Planarity graph with the specified 
+	number of vertices and with edges chosen with
+	the specified edge edge probability
+	"""
 	G = nx.fast_gnp_random_graph(vertices, edge_probability)
 	G = planarity.planarity_networkx.pgraph_graph(G)
 
 	return G
 
-for vertices in range(3, 11):
-	for i in range(5):
-		edge_probability = random.uniform(0, 0.7)
+for vertices in range(starting_vertices, ending_vertices):
+	for i in range(trials):
+		# try to make a planar graph
+		# the edge probability's equation proceeds from trial-and-error
+		edge_probability = random.uniform(0, 0.0091*(vertices**2) - 0.1488*vertices + 0.8092)
 		G = make_graph(vertices, edge_probability)
+		# continue to create graphs until we have one that is planar
 		while planarity.planarity_functions.is_planar(G) is not True:
 			G = make_graph(vertices, edge_probability)
 
+		# convert the graph into a network_x graph, and find the independent set
+		# append pertinent data to our dataset
 		G_nx = planarity.planarity_networkx.networkx_graph(G)
 		max_indep_set = len(nx.maximal_independent_set(G_nx))
 		data.append((vertices, G_nx.number_of_edges(), max_indep_set))
 
-print data
+# get rid of the old data before writing
+# then open the file, and print add the header
+os.remove(output_file) if os.path.exists(output_file) else None
+output = open(output_file, 'w')
+output.write("n(G)\te(G)\ta(G)\n")
+
+# add all data
+for trial in data:
+	output.write(str(trial[0]) + "\t" + str(trial[1]) + "\t" + str(trial[2]) + "\n")
+
+# close the file and exit
+output.close()
